9a79431828b39b8d70ab8d5632d3664e4cdb4000
Мария Журба <47789180+MariaKrepko@users.noreply.github.com>
2019-02-24 16:27:40 +0300

    Add files via upload

---
+++ table.py
@@ -0,0 +1,11 @@
+a = int(input("Введите номер элемента: ")) 
+if a == 3:
+    print("Это Li") 
+elif a == 25: 
+    print("Это Mn") 
+elif a == 80: 
+    print("Это Hg") 
+elif a == 17: 
+    print("Это Cl") 
+else: 
+    print("Попробуй еще раз") 
