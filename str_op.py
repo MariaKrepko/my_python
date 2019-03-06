2c195c714ecae00efb7a72d211f48f6595a17c53
Мария Журба <47789180+MariaKrepko@users.noreply.github.com>
2019-03-06 12:00:10 +0300

    Add files via upload

---
+++ str_op.py
@@ -0,0 +1,10 @@
+s = "У лукоморья 123 дуб зеленый 456"
+print("Индекс буквы 'я': ",s.find('я'))
+print('В списке',s.count('у'), 'буквы у')
+print(s.isalpha())
+if s.isalpha:
+    print(s.upper())
+l = int(len(s))
+if l > 4:
+    print(s.lower())
+print(s.replace('У','О'))
